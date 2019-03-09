337bd29fa790cd82508e31d44ee6fe798d3eb6a4
daniel570 <danielharsheffer@gmail.com>
2019-03-09 08:20:51 -0800

    Create tunity-provisioning.tf

---
+++ tunity-provisioning.tf
@@ -0,0 +1,37 @@
+vim tunity-provisioning.tf
+
+resource "google_compute_network" "tunity_development" {
+  name                    = "tunity-development"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "tunitycluster" {
+  name          = "tunitycluster"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = "${google_compute_network.tunity_development.self_link}"
+  region        = "europe-west1"
+}
+resource "google_container_cluster" "tunity_development_cluster" {
+  name               = "tunity-development-cluster"
+  zone               = "europe-west1-b"
+  initial_node_count = "2"
+  network            = "tunity-development"
+  subnetwork         = "tunitycluster"
+  node_config {
+    machine_type     = "n1-standard-1"
+  }
+  master_auth {
+    username = "tunity"
+    password = "tunitydevopsmaster"
+  }
+}
+
+add local-exec provisioner for running scripts:
+
+provisioner "local-exec" {
+    command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
+  }
+
+terraform init
+terraform plan
+terraform apply
