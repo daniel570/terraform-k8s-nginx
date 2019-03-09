3975ffe782df1ed8221201302691a4c1d2f29307
daniel570 <danielharsheffer@gmail.com>
2019-03-09 13:26:43 -0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,5 +5,3 @@ kubectl run nginx --image=nginx --replicas=3 --port=80
 kubectl autoscale deployment "nginx" --cpu-percent=50 --min=1 --max=10
 
 kubectl expose deployment nginx --type="LoadBalancer"
-
-kubectl get service nginx
