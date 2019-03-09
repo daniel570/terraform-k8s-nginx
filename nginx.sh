9cbe47f47a0e52745d801b203bc3d4e58f47b86b
daniel570 <danielharsheffer@gmail.com>
2019-03-09 08:26:39 -0800

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl run nginx --image=nginx --replicas=3 --port=80
+
+kubectl autoscale deployment "nginx" --cpu-percent=50 --min=1 --max=10
+
+kubectl expose deployment nginx --type="LoadBalancer"
+
+kubectl get service nginx
